46a12f65c03aa2512fec170138ebec3f86003932
Pmacdon15 <pmacdonald15@gmail.com>
2025-04-10 15:55:48 -0600

    fixed type in hook

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,14 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 import { Section } from '@/types/types';
 
-const fetchBlogSections = async (): Promise<Section[]> => {
-    const response = await fetch(`/api/blog-sections`)
+const fetchBlogSections = async (blogId: number): Promise<Section[]> => {
+    const response = await fetch(`/api/blog/${blogId}`)
     return await response.json();
 }
-export const useGetSections = () => {
+
+export const useGetSections = (blogId: number) => {
     return useQuery({
-        queryKey: ['blog-sections'],
-        queryFn: fetchBlogSections,
+        queryKey: ['blog-sections', blogId],
+        queryFn: () => fetchBlogSections(blogId),
     })
 }
+
+
 
