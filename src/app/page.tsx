747fde377905dde085b8f0268717b5480ec8c80b
Pmacdon15 <pmacdonald15@gmail.com>
2025-04-19 20:12:23 -0600

    added add blog form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AddBlogForm from "@/components/ui/add-blog-form/add-blog-form";
 import { DisplayBlogs } from "@/components/ui/display-blogs-display-blogs";
 
 export default function Home() {
@@ -6,7 +7,12 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-8 justify-start min-h-screen items-center mt-8 pb-20 font-[family-name:var(--font-geist-sans)]">
       <DisplayBlogs published={true} title="Published Blogs" isAdmin={isAdmin} />
-      {isAdmin && <DisplayBlogs published={false} title="Draft Blogs" isAdmin={isAdmin} />}
+      {isAdmin &&
+        <>
+          <DisplayBlogs published={false} title="Draft Blogs" isAdmin={isAdmin} />
+          <AddBlogForm />
+        </>
+      }
     </div>
   );
 }
