73b7defd7cd3ffc355b725bffd5257d31acc4556
Patrick MacDonald <patrick@patmac.ca>
2025-09-20 11:11:18 -0600

    removed build error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     <PageContainer>
       <div className="flex flex-col items-center justify-center text-center gap-4">
         <h2 className="text-4xl font-bold">Page Not Found</h2>
-        <p className="text-lg">We couldn't find the page you were looking for.</p>
+        <p className="text-lg">We couldn&lsquo;t find the page you were looking for.</p>
         <Link
             href="/"
             className="bg-[linear-gradient(to_bottom_right,var(--primary),var(--secondary))] border p-2 rounded-sm mx-auto hover:bg-black hover:scale-110 transition-transform duration-300"
