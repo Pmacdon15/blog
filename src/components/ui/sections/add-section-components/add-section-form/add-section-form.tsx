e84f2e764f2e4dd47da5e45479299d566ffdcd6f
Patrick MacDonald <patrick@patmac.ca>
2025-09-20 10:53:20 -0600

    converted useAddBlog to action

--- src/components/ui/sections/add-section-components/add-section-form/add-section-form.tsx
+++ src/components/ui/sections/add-section-components/add-section-form/add-section-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from "react"
 import { TitleSection } from "../title-section"
-import { useAddSection } from "@/lib/mutations/mutations";
+
 import ParagraphSection from "../paragraph-section";
 import CodeSection from "../code-section";
 import { ImageSection } from "../image-section";
--- src/lib/mutations/mutations.ts
+++ src/lib/mutations/mutations.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { createBlog, deleteBlogSection, togglePublishBlog, updateSection } from "../actions/blog-action";
 import { revalidatePathAction } from "../actions/revalidatePath-action";
 
@@ -21,7 +21,7 @@ export const useTogglePublishBlog = (blogId: number) => {
 };
 
 
-export const useAddBlog = () => {    
+export const useAddBlog = () => {
     return useMutation({
         mutationFn: (formData: FormData) => {
             return createBlog(formData);
