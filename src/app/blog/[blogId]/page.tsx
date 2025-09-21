f96ce030276353836992695b94b05695929f1727
Patrick MacDonald <patrick@patmac.ca>
2025-09-21 17:07:03 -0600

    updated params using page props

--- src/app/blog/[blogId]/page.tsx
+++ src/app/blog/[blogId]/page.tsx
@@ -12,16 +12,10 @@ export async function generateStaticParams() {
     return blogIds;
 }
 
-export default async function BlogPage({
-    params,
-}: {
-    params: { blogId: string }
-}) {
-    const sections = await getSections(params.blogId);
+export default async function BlogPage(props: PageProps<'/blog/[blogId]'>) {
+    const sections = await getSections((await props.params).blogId);
 
-    if (!sections || 'error' in sections || sections.length === 0) {
-        notFound();
-    }
+    if (!sections || 'error' in sections || sections.length === 0) notFound();
 
     return (
         <Suspense fallback={<div>Loading sections...</div>}>
--- src/app/edit-blog/[blogId]/page.tsx
+++ src/app/edit-blog/[blogId]/page.tsx
@@ -3,16 +3,10 @@ import { getSections } from "@/lib/DAL/blogs-dal";
 import { Suspense } from "react";
 import { notFound } from 'next/navigation';
 
-export default async function BlogPage({
-    params,
-}: {
-    params: { blogId: string }
-}) {
-    const sections = await getSections(params.blogId);
+export default async function BlogPage(props: PageProps<'/edit-blog/[blogId]'>) {
+    const sections = await getSections((await props.params).blogId);
 
-    if (!sections || 'error' in sections || sections.length === 0) {
-        notFound();
-    }
+    if (!sections || 'error' in sections || sections.length === 0) notFound();
 
     return (
         <Suspense fallback={<div>Loading sections...</div>}>
