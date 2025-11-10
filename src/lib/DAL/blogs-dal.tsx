451319c2ab1cca364c49e41c4e6ea28064fea93a
Patrick MacDonald <patrick@patmac.ca>
2025-11-09 22:31:01 -0700

    added todo

--- src/lib/DAL/blogs-dal.tsx
+++ src/lib/DAL/blogs-dal.tsx
@@ -82,38 +82,7 @@ export async function getSections(
   'use cache'
 	try {
 		const sql = neon(`${process.env.DATABASE_URL}`)
-
-		// const sectionsQuery = `
-    //         SELECT 
-    //             S.id,
-    //             S.blog_id,
-    //             S.type as section_type_id,
-    //             S.order_index,
-    //             CASE S.type
-    //                 WHEN 1 THEN 'title'
-    //                 WHEN 2 THEN 'image'
-    //                 WHEN 3 THEN 'paragraph'
-    //                 WHEN 4 THEN 'code'
-    //             END as section_type,
-    //             TS.title as title_section_title,
-    //             TS.publish_date,
-    //             I.src,
-    //             I.alt,
-    //             I.width,
-    //             P.title as paragraph_title,
-    //             P.text,
-    //             C.language,
-    //             C.code,
-    //             B.published
-    //         FROM Section S
-    //         LEFT JOIN TitleSection TS ON S.id = TS.id AND S.type = 1
-    //         LEFT JOIN ImageSection I ON S.id = I.id AND S.type = 2
-    //         LEFT JOIN ParagraphSection P ON S.id = P.id AND S.type = 3
-    //         LEFT JOIN CodeSection C ON S.id = C.id AND S.type = 4
-    //         JOIN Blog B ON S.blog_id = B.id
-    //         WHERE S.blog_id = ${blogId}
-    //         ORDER BY S.order_index ASC
-    //     `
+// TODO: Make sure cache is invalidated maybe add a tag
 		const result = await sql`
             SELECT 
                 S.id,
