e14c4de30ce6241d35df3abc600fc752120e5dff
Patrick MacDonald <patrick@patmac.ca>
2025-11-27 22:09:07 -0700

    fixed build errors

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,7 +3,7 @@ import BackHomeLink from '@/components/ui/links/back-home-link'
 import { getBlogs } from '@/lib/DAL/blogs-dal'
 
 export default function Page() {
-	const blogsPromise = getBlogs(true)
+	const blogsPromise = getBlogs()
 	return (
 		<div className="flex w-full flex-col items-center gap-8">
 			<Blogs blogsPromise={blogsPromise} />
