d86246181a23ae6e8a708ab14339b7086c9eb191
Patrick MacDonald <patrick@patmac.ca>
2026-01-26 17:19:15 -0700

    allow site map to be viewed

--- src/proxy.ts
+++ src/proxy.ts
@@ -32,7 +32,7 @@ import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware'
 
 export default withAuth(async function middleware(_req: Request) {}, {
 	// Middleware still runs on all routes, but doesn't protect the blog route
-	publicPaths: ['/blog', '/'],
+	publicPaths: ['/blog', '/' ,'/sitemap*'],
 })
 
 export const config = {
